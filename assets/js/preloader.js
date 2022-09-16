802ff5a75e1550a27bfd8a1f52e749aef50f9dec
amitsaxena999 <aamitsaxenaa1@gmail.com>
2022-09-16 16:34:29 +0530

    initial project push

---
+++ assets/js/preloader.js
@@ -0,0 +1,65 @@
+
+/* 
+-------------------------------
+Controlling Header Transparency 
+-------------------------------
+*/
+function transparent_header()
+{
+    if ( $(window).scrollTop() < parseInt( (window.innerHeight * 0.12), 10 ) ) {
+        $('.kayden-header > .header-inside').addClass('transparent_header');
+    }else {
+        $('.kayden-header > .header-inside').removeClass('transparent_header'); 
+    }
+}
+/* 
+-------------------------------
+Before loading function. 
+-------------------------------
+*/
+function loading()
+{
+    var scrollerwidth = window.innerWidth - document.documentElement.clientWidth;
+
+    document.body.style.overflow = 'hidden';
+    document.body.style.marginRight = scrollerwidth + "px";
+
+    window.scrollTo(0,0);
+
+}
+
+/* 
+-------------------------------
+After loaded function. 
+-------------------------------
+*/
+
+function loaded(elem)
+{
+    
+    $preloader = $("#preloader");
+
+    var event = new CustomEvent('kaydenLoaded');
+
+    $preloader.fadeOut( 400, function(){
+
+        $('body').removeAttr('style');
+
+        $('body').addClass('loaded');
+
+        elem.dispatchEvent(event);
+    });
+}
+
+loading();
+
+/* 
+-------------------------------
+On Page Load. 
+-------------------------------
+*/
+$(window).on('load', function(){
+    loaded( document.body );
+});
+
+
